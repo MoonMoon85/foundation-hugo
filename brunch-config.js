366dd9b12da11e3a734d58204de8a754cbffd888
Torleif Nielsen <torleif@hawaii.edu>
2016-12-24 16:01:52 -0800

    Refactor foundation into node modules and compile with Brunch

--- brunch-config.js
+++ brunch-config.js
@@ -1,7 +1,23 @@
 module.exports = {
-  paths: { public: 'static' },
+  paths: { public: "static" },
   files: {
-    javascripts: { joinTo: 'app.js'  },
-    stylesheets: { joinTo: 'app.css' }
+    javascripts: { joinTo: "app.js" },
+    stylesheets: { joinTo: "app.css" }
+  },
+  plugins: {
+    sass: {
+      options: {
+        includePaths: [
+          "node_modules/foundation-sites/scss",
+          "node_modules/motion-ui/src"
+        ]
+      }
+    },
+  },
+  npm: {
+    globals: {
+      $: 'jquery',
+      jQuery: 'jquery'
+    }
   }
 }
